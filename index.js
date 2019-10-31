ad172d7407f3026688e53793a7fecc46c7e64cd0
Asen Chekov <asenchekov@users-mbp.c.machine-learning-200712.internal>
2019-10-31 12:02:17 +0000

    Initial commit

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ const TOKEN = process.env.TOKEN;
 bot.login(TOKEN);
 
 bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user.tag}!`);
+  console.info(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-        msg.channel.send('pong');
-
-    } else if (msg.content.startsWith('!kick')) {
-        if (msg.mentions.users.size) {
-            const taggedUser = msg.mentions.users.first();
-            msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-        } else {
-            msg.reply('Please tag a valid user!');
-        }
-    }
+  if ((msg.content.split(' ').includes('mic')
+    || msg.content.split(' ').includes('microphone'))
+    && msg.author.id !== '639388710990774283') {
+      let responses = [
+        "your mic is the worst.\nSeriously let me buy one for you.",
+        "it's really bad, buy new one and send me the receipt man.",
+        "it's about time you buy a proper mic."
+      ];
+      msg.reply(responses[Math.floor(Math.random() * responses.length)]);
+  } else if (msg.content.split(' ').includes('headphones')
+      && msg.author.id !== '639388710990774283') {
+        let responses = [
+          "your headphones are the worst.\nSeriously let me buy one for you.",
+          "it's really bad, buy new one and send me the receipt man.",
+          "it's about time you buy a proper headset."
+        ];
+        msg.reply(responses[Math.floor(Math.random() * responses.length)]);
+  }
 });
